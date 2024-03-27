e6ff69a114351e55aac1fc85417781b5c7774e0c
Shlok Bharuka <shlokbharuka01@gmail.com>
2024-03-27 21:10:44 +0530

    manage course

--- website/models.py
+++ website/models.py
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
     submission = db.relationship('Submission')
     enroll = db.relationship('Enroll')
     
-class Assignment(db.Model):
+class Assignment(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(1000000000000000000000000000000))
     assign_date = db.Column(db.DateTime(timezone=True), default=func.now())
@@ -25,29 +25,30 @@ class Assignment(db.Model):
     file = db.Column(db.String(150))
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
 
-class Course(db.Model):
+class Course(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     description = db.Column(db.String(100))
     creator = db.Column(db.Integer, db.ForeignKey('user.id'))
-    enroll_id = db.Column(db.Integer) 
+    enroll_id = db.Column(db.Integer)
     material = db.relationship('Material')
     notification = db.relationship('Notification')
     assignment = db.relationship('Assignment')
     enroll = db.relationship('Enroll')
 
-class Enroll(db.Model):
+class Enroll(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-class Discussion(db.Model):
+    
+class Discussion(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(1000))
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     timestamp = db.Column(db.DateTime(timezone=True), default=func.now())
 
-class Material(db.Model):
+class Material(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(1000))
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
@@ -55,13 +56,13 @@ class Material(db.Model):
     timestamp = db.Column(db.DateTime(timezone=True), default=func.now())
     file = db.Column(db.String(150))
 
-class Notification(db.Model):
+class Notification(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     message = db.Column(db.String(100))
 
-class Submission(db.Model):
+class Submission(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     assignment_id = db.Column(db.Integer, db.ForeignKey('assignment.id'))
     submitter = db.Column(db.Integer, db.ForeignKey('user.id'))
